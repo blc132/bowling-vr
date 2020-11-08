a8e27f1d3ce1ad98cb9db431f09f6fe0f662721c
DESKTOP-53OQFUF\Szymon <deesugfx@gmail.com>
2020-11-08 15:38:15 +0100

    Dodano kulę do kręgli Dodano materiał do kuli do kręgli Dodano skrypt gracza do kamery Zmieniono nazwę kamery na "Player" Dodano skrypt poruszania kamerą przy użyciu myszki Dodano przyklejanie się kuli do aktualnej pozycji myszki (symulacja trzymania kuli przed sobą) Dodano funkcjonalność wyrzucania kuli do kręgli przed siebie

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bowlingBall;
+    public float ballDistance = 2.5f;
+    public float ballThrowingForce = 200f;
+
+    private bool holdingBowlingBall = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (holdingBowlingBall)
+        {
+            bowlingBall.transform.position = transform.position + transform.forward * ballDistance;
+
+            if (Input.GetKeyDown("space"))
+            {
+                ThrowBowlingBall();
+            }
+        }
+    }
+
+    private void ThrowBowlingBall()
+    {
+        holdingBowlingBall = false;
+
+        var bowlingBallRigidBody = bowlingBall.GetComponent<Rigidbody>();
+
+        bowlingBallRigidBody.useGravity = true;
+        bowlingBallRigidBody.AddForce(transform.forward * ballThrowingForce);
+    }
+}
