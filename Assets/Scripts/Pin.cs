48660cbb32266db80250c69cb3f43fd2ae635fc1
DESKTOP-53OQFUF\Szymon <deesugfx@gmail.com>
2020-12-07 12:10:55 +0100

    Dodanie dźwięku zbijanego kręgla

--- Assets/Scripts/Pin.cs
+++ Assets/Scripts/Pin.cs
@@ -10,22 +10,36 @@ public class Pin : MonoBehaviour
     /// </summary>
     public bool remove;
     // Start is called before the first frame update
+
+    public AudioSource bowlingBallHitPinSource;
     void Start()
     {
-
+        bowlingBallHitPinSource = GetComponent<AudioSource>();
+        bowlingBallHitPinSource.playOnAwake = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(remove)
+        if (remove)
             Destroy(gameObject);
     }
 
     public void OnTouchFloor()
     {
         Debug.Log("Touched floor");
-        if(gameObject != null)
+        if (gameObject != null)
             Destroy(gameObject);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        var colliderTag = collision.collider.tag;
+        if (colliderTag == "BowlingBall" || colliderTag == "Pin" && !bowlingBallHitPinSource.isPlaying)
+        {
+
+            Debug.Log("Ball hit the pin");
+            bowlingBallHitPinSource.Play();
+        }
     }
 }
